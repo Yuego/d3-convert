c700ad5ed7963fa6982dac80adb6c19ecc28b9bd
Yuego [Lenovo] <root@proscript.ru>
2014-02-24 18:25:23 +0400

    version info update

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: Russian',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
