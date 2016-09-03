ecaddf90c765632d745105737c4af479c179b488
Vlasov Artem [Lenovo] <root@proscript.ru>
2016-09-03 12:59:15 +0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+import sys
+
 from d3_convert.version import __version__
+
+extra_requires = []
+
+PY3 = sys.version_info[0] == 3
+PY35_plus = PY3 and sys.version_info[1] >= 5
+
+if not PY35_plus:
+    extra_requires.append('scandir')
 
 setup(
     name='d3_convert',
     version=__version__,
     author='Artem Vlasov',
     author_email='root@proscript.ru',
-    url='',
+    url='https://github.com/Yuego/d3-convert',
 
     description='D3 Converter',
     long_description=open('README.rst').read(),
@@ -19,9 +29,8 @@ setup(
         #'PyGObject',
         # media-libs/gexiv2
         'lxml',
-        #'scandir',
         'psutil>=2',
-    ],
+    ] + extra_requires,
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     classifiers=[
@@ -33,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
